a3275172bff81b721fff0a0fe0ea7297cdec2ec2
Anton Crona <crona.anton@gmail.com>
2021-06-07 11:55:28 +0100

    Added a print statement to a new branch. Let's see if this will be pushed...

--- src/main/java/com/wheeliebin/MainBin.java
+++ src/main/java/com/wheeliebin/MainBin.java
@@ -1,4 +1,13 @@
 package wheeliebin;
 
 public class MainBin {
+
+    public static void main(String[] args) {
+        System.out.println("Something for GitLab branching...");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
